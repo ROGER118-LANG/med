b03ed2d710a3206e47c8fc6cfb05af48b6af86f9
ROGER118-LANG <84017047+ROGER118-LANG@users.noreply.github.com>
2024-10-06 18:03:47 -0300

    app.py

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ def load_models():
     labels_paths = ["labels_tuberculose.txt", "labels_pneumonia.txt", "labels_outro.txt"]
     
     models = {}
-    for model_path, labels_path in zip(model_paths, labels_paths):
-model = load_model(model_path, compile=False)
+  for model_path in model_paths:  # Certifique-se de que model_paths esteja definido
+    model = load_model(model_path, compile=False
 
         with open(labels_path, "r") as f:
             class_names = [line.strip() for line in f.readlines()]
