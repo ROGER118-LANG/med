4c50f00f83a09bfefc40c674caa5654524423720
ROGER118-LANG <84017047+ROGER118-LANG@users.noreply.github.com>
2024-10-06 14:46:16 -0300

    app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def load_models():
     models = {}
     disease_configs = {
         "Tuberculose": {
-            "model": "ROGER118-LANG/med/blob/main/pneumonia_model.h5",
+            "model": "pneumonia_model.h5",
             "labels": "tuberculose_labels.txt"
         },
         "Câncer": {
